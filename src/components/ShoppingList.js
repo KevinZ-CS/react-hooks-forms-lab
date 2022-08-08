bd4d46c0226cd39571384be8154ad07a685dc1b2
Kevin Zheng <kevzheng27@gmail.com>
2022-08-07 22:10:41 -0400

    commit tree 9afce91c4891c5cfa87bdd4633cb52dd71cd722f

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,27 +3,55 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [search, setSearch] = useState('');
+
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
+  const itemsToDisplay = items
+  // category
+  .filter(
+    (item) => selectedCategory === "All" || item.category === selectedCategory
+  )
+  // search term
+  .filter((item) => item.name.toLowerCase().includes(search.toLowerCase()));
 
-    return item.category === selectedCategory;
-  });
+
+
+//   const itemsToDisplay = items.filter((item) => {
+//     if (selectedCategory === "All") return true;
+
+//     return item.category === selectedCategory;
+//   });
+
+//  const searchDisplay = items.filter((item) => {
+//     if(search.length > 0 && item.name.includes(search)) {return true}
+//     else {return false}
+//   } 
+//  )
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} />
+      <Filter search={search} onCategoryChange={handleCategoryChange} onSearchChange={setSearch} />
       <ul className="Items">
-        {itemsToDisplay.map((item) => (
+      {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
+
+
+        {/* {searchDisplay.length > 0
+        ? searchDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        )) :
+        itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))} */}
       </ul>
     </div>
   );
